dbb61036ac12d248f0ccaa2c9358ff01f3491dea
andrewgodbout <agodbout@upei.ca>
2025-09-17 13:32:47 -0300

    homework 3

---
+++ homework/HW3/tests/q3.py
@@ -0,0 +1,11 @@
+OK_FORMAT = True
+
+test = {   'name': 'q3',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': '>>> import math\n>>> math.isclose(numerator, 0.01126, abs_tol=0.0001)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> import math\n>>> math.isclose(denominator, 0.1268, abs_tol=0.0001)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> import math\n>>> math.isclose(monthly_payment, 88.85, abs_tol=0.01)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ homework/HW3/tests/q4.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q4',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> buy_property == True or buy_property == False\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
