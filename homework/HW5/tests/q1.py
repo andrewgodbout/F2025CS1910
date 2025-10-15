4541e599e24b4dbb18743098e2e5c14edea064a6
andrewgodbout <agodbout@upei.ca>
2025-10-15 09:31:55 -0300

    hw5

---
+++ homework/HW5/tests/q1.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'q1', 'points': 1, 'suites': [{'cases': [{'code': ">>> q1_option == 'B'\nTrue", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ homework/HW5/tests/q2.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'q2', 'points': 1, 'suites': [{'cases': [{'code': '>>> branch_count == 8\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
