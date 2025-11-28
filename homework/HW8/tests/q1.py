c01511e49634052876db6686e2f7f5c1cfca87e0
andrewgodbout <agodbout@upei.ca>
2025-11-28 14:58:47 -0400

    hw8

--- homework/HW8/tests/q1.py
+++ homework/HW8/tests/q1.py
@@ -1,28 +1,31 @@
-from otter.test_files import test_case
+OK_FORMAT = True
 
-OK_FORMAT = False
-
-name = "q1"
-points = 2
-
-@test_case(points=1, hidden=False)
-def t1():
-    pb = {}
-    ph1 = "902-555-5555"
-    add_contact(pb, "andrew", ph1)
-    ph2 = get_contact(pb, "andrew")
-    assert ph1 == ph2
-
-t1()
-@test_case(points=1, hidden=False)
-def t2():
-    pb = {}
-    ph1 = "902-555-5554"
-    add_contact(pb, "sally", ph1)
-    add_contact(pb, "george", "999-999-9999")
-    save(pb, "a.txt")
-    pb2 = load_contacts("a.txt")
-    ph2 = get_contact(pb2,"sally")
-    assert ph1 == ph2
-
-t2()
+test = {   'name': 'q1',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> def t1():\n'
+                                               '...     pb = {}\n'
+                                               "...     ph1 = '902-555-5555'\n"
+                                               "...     add_contact(pb, 'andrew', ph1)\n"
+                                               "...     ph2 = get_contact(pb, 'andrew')\n"
+                                               '...     return ph1 == ph2\n'
+                                               '>>> t1()\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> def t2():\n'
+                                               '...     pb = {}\n'
+                                               "...     ph1 = '902-555-5554'\n"
+                                               "...     add_contact(pb, 'sally', ph1)\n"
+                                               "...     add_contact(pb, 'george', '999-999-9999')\n"
+                                               "...     save(pb, 'a.txt')\n"
+                                               "...     pb2 = load_contacts('a.txt')\n"
+                                               "...     ph2 = get_contact(pb2, 'sally')\n"
+                                               '...     return ph1 == ph2\n'
+                                               '>>> t2()\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
